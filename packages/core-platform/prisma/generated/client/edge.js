5e89165562b30b409340ec9c25a128f6884c96db
Gbemi'king <tosinjaiyeoba@yahoo.com>
2026-09-09 12:20:29 +0100

    fix: resolve Render deployment pipeline and entrypoint module resolution

--- packages/core-platform/prisma/generated/client/edge.js
+++ packages/core-platform/prisma/generated/client/edge.js
@@ -1663,8 +1663,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../..",
   "clientVersion": "5.22.0",
@@ -1673,6 +1672,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
